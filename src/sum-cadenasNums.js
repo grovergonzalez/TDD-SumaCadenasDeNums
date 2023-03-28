2c525358fd681963e314cde8e9c39c795477b299
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-03-28 00:27:15 -0400

    Se modifica la funcion para pasar el ultimo test

--- src/sum-cadenasNums.js
+++ src/sum-cadenasNums.js
@@ -1,12 +1,28 @@
 function SumCadenasDeNums(cadenaDeNums)
 {
     let SumatoriaCadenas = 0;
+    let caracterSig  = "";
+    if(cadenaDeNums[2] == "["){
+        caracterSig = cadenaDeNums[3];
+    }
     for(let i =0;i<cadenaDeNums.length;i++){
-        if(cadenaDeNums[i]=="," || cadenaDeNums[i]=="-"){
-            SumatoriaCadenas+=0;
+        
+        if(caracterSig == "")
+        {
+            if(cadenaDeNums[i]=="," || cadenaDeNums[i]=="-"){
+                SumatoriaCadenas+=0;
+            }
+            else{
+                SumatoriaCadenas +=+cadenaDeNums[i];
+            }
         }
         else{
-            SumatoriaCadenas +=+cadenaDeNums[i];
+            if(cadenaDeNums[i+7] == "," || cadenaDeNums[i+7] == "-" || cadenaDeNums[i+7] == caracterSig) {
+                SumatoriaCadenas += 0;
+            } 
+            else{
+                SumatoriaCadenas += +cadena[i+7];
+            }
         }
     }
     return SumatoriaCadenas;
--- src/sum-cadenasNums.spec.js
+++ src/sum-cadenasNums.spec.js
@@ -16,13 +16,13 @@ describe("Sumar", () => {
   it("deberia devolver sumar 2 valores separados por guion", () => {
     expect(SumCadenasDeNums("1-2")).toEqual(3);
   });
-  xit("deberia devolver sumar mas de 2 valores por guion", () => {
+  it("deberia devolver sumar mas de 2 valores por guion", () => {
     expect(SumCadenasDeNums("1-2-3-4-5")).toEqual(15);
   });
-  xit("deberia devolver sumar mas de 2 valores separado por guion o coma", () => {
+  it("deberia devolver sumar mas de 2 valores separado por guion o coma", () => {
     expect(SumCadenasDeNums("1,2,3-4-5")).toEqual(15);
   });
-  xit("deberia devolver suma de 2 valores separados por guion o coma o un delimitardor especial ", () => {
+  it("deberia devolver suma de 2 valores separados por guion o coma o un delimitardor especial ", () => {
     expect(SumCadenasDeNums("//[;] 6,3-2;1")).toEqual(12);
   });
 });
