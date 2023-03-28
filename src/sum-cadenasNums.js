4386455209547315739a785aec7b28ae394c55a8
grovergonzalez <grover.gonzalez@ucb.edu.bo>
2023-03-27 23:42:19 -0400

    Creamos la primera version de la funcion para pasar el primer test

---
+++ src/sum-cadenasNums.js
@@ -0,0 +1,6 @@
+function SumCadenasDeNums(cadena)
+{
+    return 0;
+}
+
+export default SumCadenasDeNums;
--- src/sum-cadenasNums.spec.js
+++ src/sum-cadenasNums.spec.js
@@ -1,26 +1,28 @@
+import SumCadenasDeNums from "./sum-cadenasNums";
+
 describe("Sumar", () => {
   it("deberia devolver el valor 0", () => {
-    expect(sumarCadenas("")).toEqual(0);
+    expect(SumCadenasDeNums("")).toEqual(0);
   });
   xit("deberia devolver la suma de 1", () => {
-    expect(sumarCadenas("1")).toEqual(1);
+    expect(SumCadenasDeNums("1")).toEqual(1);
   });
   xit("deberia devolver la suma de 1 y 2", () => {
-    expect(sumarCadenas("1,2")).toEqual(3);
+    expect(SumCadenasDeNums("1,2")).toEqual(3);
   });
   xit("deberia devolver la suma de 2 o mas valores", () => {
-    expect(sumarCadenas("1,2,3,4,5")).toEqual(15);
+    expect(SumCadenasDeNums("1,2,3,4,5")).toEqual(15);
   });
   xit("deberia devolver sumar 2 valores separados por guion", () => {
-    expect(sumarCadenas("1-2")).toEqual(3);
+    expect(SumCadenasDeNums("1-2")).toEqual(3);
   });
   xit("deberia devolver sumar mas de 2 valores por guion", () => {
-    expect(sumarCadenas("1-2-3-4-5")).toEqual(15);
+    expect(SumCadenasDeNums("1-2-3-4-5")).toEqual(15);
   });
   xit("deberia devolver sumar mas de 2 valores separado por guion o coma", () => {
-    expect(sumarCadenas("1,2,3-4-5")).toEqual(15);
+    expect(SumCadenasDeNums("1,2,3-4-5")).toEqual(15);
   });
   xit("deberia devolver suma de 2 valores separados por guion o coma o un delimitardor especial ", () => {
-    expect(sumarCadenas("//[;] 6,3-2;1")).toEqual(12);
+    expect(SumCadenasDeNums("//[;] 6,3-2;1")).toEqual(12);
   });
 });
